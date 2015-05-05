eb1a023d0963cc2737730e63d7fc2d1523370e1b
Mapuyc <vladislav1733@gmail.com>
2015-05-05 13:55:15 +0300

    modal login2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SchoolApp::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {sessions: 'sessions', registrations: 'registrations'}
   root 'static_pages#main'
-  get "static_pages/visit"
+  get "static_pages/visit", as: 'user_root'
   get "static_pages/mbank"
   get "static_pages/multrp"
   get "static_pages/gwork"
