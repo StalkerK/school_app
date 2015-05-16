e07edbf3329167489380d64557b6c04b995deec0
xwzsd <shamik2024@gmail.com>
2015-05-16 10:06:05 +0300

    add_paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
 	
 	validates_presence_of :body, :title
+	has_attached_file :photo, styles: {large: "600x600>", medium: "300x300>", thumb: "150x150#"}
+	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+  
 end
