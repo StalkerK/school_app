917537a8360c2ebd694508750233bea289e03427
xwzsd <shamik2024@gmail.com>
2015-06-01 16:04:35 +0300

    little fix galleria

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,4 +1,5 @@
 class UploadsController < ApplicationController
+  before_filter :authenticate_user!, :except => :index
   before_action :set_upload, only: [:show, :edit, :update, :destroy]
 
   # GET /uploads
