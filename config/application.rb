4b73eeb22e62ae37442eb88a527b49f8800cbc73
xwzsd <shamik2024@gmail.com>
2015-06-25 11:16:53 +0300

    dx

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module SchoolApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
-    config.assets.precompile = %w(*.png *.jpg *.jpeg *.gif *.css.scss)
+    config.assets.precompile = %w(*.png *.jpg *.jpeg *.gif *.css.scss application.css *.js)
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
