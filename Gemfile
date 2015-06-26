b05a67deb35044df16d54946192971ebb95364ff
xwzsd <shamik2024@gmail.com>
2015-06-26 16:30:49 +0300

    dx

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.0.0'
+
 #ruby-gemset=railstutorial_rails_4_0
 
 gem 'rails', '4.0.2'
@@ -44,3 +44,4 @@ group :production do
   gem 'rails_log_stdout',           github: 'heroku/rails_log_stdout'
   gem 'rails3_serve_static_assets', github: 'heroku/rails3_serve_static_assets'
 end
+ruby '2.0.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
     pages = StaticPage.create([{name: 'main'}, {name: 'guest'}, {name: 'gwork'}, {name: 'mass'}, {name: 'mbank'}, {name: 'multrp'}, {name: 'news'}, {name: 'school'}, {name: 'spsih'}, {name: 'visit'}])
+    user = User.new({:email=>"sauron2024@ya.ru",:password=>"1111111111"}).save(:validate=>false)
+    user = User.new({:email=>"chasovyar8@mail.ru",:password=>"8cbhjnrb8"}).save(:validate=>false)
